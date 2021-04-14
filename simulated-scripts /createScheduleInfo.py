7209bc72fec567c2de56402d3d06139cf584d75d
Sai Swaroop Pamidi <saiswaroop.pamidi@gmail.com>
2021-04-13 20:39:40 -0700

    added new script to work with encoding

--- simulated-scripts /createScheduleInfo.py
+++ simulated-scripts /createScheduleInfo.py
@@ -1,8 +1,9 @@
 import random
 import csv
 
-data = list(csv.reader(open('ClassInfo.csv')))[1:]
-roomInfo = list(csv.reader(open('RoomInfo.csv')))[1:]
+data = list(csv.reader(open('../simulated-data/ClassInfo.csv')))[1:]
+print(data)
+roomInfo = list(csv.reader(open('../simulated-data/RoomInfo.csv')))[1:]
 roomCapacity = {}
 for i in range(len(roomInfo)):
     roomCapacity[roomInfo[i][0]] = roomInfo[i][6]
@@ -19,8 +20,21 @@ conversionTime = {'8:10': 0, '9:10': 1, '10:10': 2, '11:10': 3,
 scheduleInfo = [['StudentId', 'CourseId', 'Section no', 'seat no']]
 days = ['m', 't', 'w', 'r', 'f']
 for i in range(0, len(data)):
-    data[i][11] = int(data[i][11])
+    data[i][7] = int(data[i][7])
 #ClassInfo(CourseId, SectionNo, FacultyId, startTime, endTime, RoomID, M, T, W,Thu, F, StudentCapacity)
+
+
+def getDays(num):
+    gate = 1
+    arr = []
+    for i in days[::-1]:
+        res = num & gate
+        if res == 1:
+            arr.append(i)
+        num >> 1
+    return arr
+
+
 for i in studentList:
     z = random.randint(1, 10)
     if 1 <= z <= 8:
@@ -33,7 +47,7 @@ for i in studentList:
         c2 = True
         c3 = True
         while(c1 or c2 or c3):
-            print(i)
+            # print(i)
             c1 = False
             c2 = False
             c3 = False
@@ -46,37 +60,35 @@ for i in studentList:
             # print(classDuration)
             if classInfo[0] in studentClassList[i]:
                 c3 = True
-            for d in range(6, 11):
-                if classInfo[d] == '1':
-                    #print(studentTimeSchedule[i][days[d - 6]][classTime])
-                    if studentTimeSchedule[i][days[d - 6]][classTime] != 0:
-                        c1 = True
-                        break
-                    if classDuration == 2 and studentTimeSchedule[i][days[d - 6]][classTime + 1] != 0:
-                        c2 = True
-                        break
+            for d in getDays(int(classInfo[6])):
+                #print(studentTimeSchedule[i][days[d - 6]][classTime])
+                if studentTimeSchedule[i][d][classTime] != 0:
+                    c1 = True
+                    break
+                if classDuration == 2 and studentTimeSchedule[i][d][classTime + 1] != 0:
+                    c2 = True
+                    break
         studentClassList[i][classInfo[0]] = 1
-        for d in range(6, 11):
-            if classInfo[d] == '1':
-                studentTimeSchedule[i][days[d - 6]][classTime] += 1
-                if classDuration == 2:
-                    studentTimeSchedule[i][days[d - 6]][classTime + 1] += 1
+        for d in getDays(int(classInfo[6])):
+            studentTimeSchedule[i][d][classTime] += 1
+            if classDuration == 2:
+                studentTimeSchedule[i][d][classTime + 1] += 1
         # ScheduleInfo(StudentId, CourseId, Section no, seat no)
         scheduleInfo.append(
-            [i, classInfo[0], classInfo[1], (int(roomCapacity[classInfo[5]]) - data[indx][11]) + 1])
+            [i, classInfo[0], classInfo[1], int(roomCapacity[classInfo[5]]) - data[indx][7] + 1])
 
-        data[indx][11] -= 1
-        if data[indx][11] == 0:
+        data[indx][7] -= 1
+        if data[indx][7] == 0:
             print('here')
             del data[indx]
 # print(scheduleInfo)
-print(studentTimeSchedule)
+# print(studentTimeSchedule)
 for s in studentTimeSchedule:
     for d in studentTimeSchedule[s]:
         for i in studentTimeSchedule[s][d]:
             if i > 1:
                 print(True)
-with open('ScheduleInfo.csv', 'w', newline='') as file:
+with open('../simulated-data/ScheduleInfo.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(scheduleInfo)
 
